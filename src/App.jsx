2de278513ebec1c84b21ffbe05d172f00cf16038
Ranjithkumar <ranjithsr.tech@gmail.com>
2025-01-04 13:17:03 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Ranjit Portfolio</h1>
+      <h1>Ranjith Portfolio to show case</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
