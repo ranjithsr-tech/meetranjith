b5e11697059e22ca89fce53f7bcf12fcad4a23f2
Ranjithkumar <ranjithsr.tech@gmail.com>
2025-01-04 12:41:31 +0530

    rename repo

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React By Ranjith</h1>
+      <h1>Ranjit Portfolio</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
